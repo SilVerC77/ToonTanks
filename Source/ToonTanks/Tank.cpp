19502e9f20eb64e4671ea2c71545555f167e5da0
SilVerC77 <silverc777@hotmail.com>
2021-12-11 15:53:55 +0900

    Added rotation and binded

--- Source/ToonTanks/Tank.cpp
+++ Source/ToonTanks/Tank.cpp
@@ -2,4 +2,35 @@
 
 
 #include "Tank.h"
+#include "Camera/CameraComponent.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Kismet/GameplayStatics.h"
 
+ATank::ATank() {
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetupAttachment(RootComponent);
+
+	Camera = CreateDefaultSubobject <UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm);
+}
+
+void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) {
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &ATank::Move);
+	PlayerInputComponent->BindAxis(TEXT("Turn"), this, &ATank::Turn);
+}
+
+void ATank::Move(float _value) {
+	FVector DeltaLocation(0.f);
+	DeltaLocation.X = _value * Speed * UGameplayStatics::GetWorldDeltaSeconds(this);
+	AddActorLocalOffset(DeltaLocation, true);
+	//UE_LOG(LogTemp, Warning, TEXT("Value:%f"), _value);
+}
+
+void ATank::Turn(float _value) {
+	FRotator DeltaRotation = FRotator::ZeroRotator;
+	DeltaRotation.Yaw = _value * TurnRate * UGameplayStatics::GetWorldDeltaSeconds(this);
+	AddActorLocalRotation(DeltaRotation);
+	//UE_LOG(LogTemp, Warning, TEXT("Value:%f"), _value);
+}
--- Source/ToonTanks/Tank.h
+++ Source/ToonTanks/Tank.h
@@ -6,12 +6,33 @@
 #include "BasePawn.h"
 #include "Tank.generated.h"
 
+class USpringArmComponent;
+class UCameraComponent;
+
 /**
- * 
+ *
  */
 UCLASS()
 class TOONTANKS_API ATank : public ABasePawn
 {
 	GENERATED_BODY()
-	
+
+public:
+	ATank();
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+private:
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+		USpringArmComponent* SpringArm;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+		UCameraComponent* Camera;
+
+	UPROPERTY(EditAnywhere, Category = "Movement")
+		float Speed = 200.f;
+	UPROPERTY(EditAnywhere, Category = "Movement")
+		float TurnRate = 45.f;
+
+	void Move(float _value);
+	void Turn(float _value);
 };
